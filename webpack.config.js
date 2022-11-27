8696cf7b6869777a438f74e5e7da3aac091b55fb
foxhsx <932163475@qq.com>
2022-11-27 16:59:56 +0800

    feat: CSS 压缩

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-const { basename } = require('path');
+const { basename, resolve } = require('path');
 const htmlPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const postcssPresetEnv = require('postcss-preset-env');
+// const postcssPresetEnv = require('postcss-preset-env');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   entry: './src/main.js',
@@ -21,12 +22,19 @@ module.exports = {
     ]
   },
   plugins: [
-    new htmlPlugin({ title: basename(__dirname) }),
+    new htmlPlugin({
+      template: './index.html'
+    }),
     new MiniCssExtractPlugin({
       filename: '[name].css',
       // chunkFilename: '[id].css'
-    })
+    }),
   ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
   optimization: {
     splitChunks: {
       chunks: 'all',
@@ -40,10 +48,16 @@ module.exports = {
         // 可以选择拼接 cacheGroupKey 也可以不拼接，甚至 allChunksNames 也可以不需要
         // `${cacheGroupKey}-${allChunksNames}-${moduleFileName}`
         // slice 可以得到这样更容易让人接受的文件名 react.development.js
+        // 最终生成的名称 moduleFileName + .js，这也是为啥要写 slice 的原因 - react.development.js.js
         return moduleFileName.slice(0, -3);
       },
     },
     runtimeChunk: true,
+    minimizer: [
+      `...`,
+      new CssMinimizerPlugin(),
+    ],
+    minimize: true
   },
   mode: 'development'
 }
